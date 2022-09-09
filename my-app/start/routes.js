b8b4eaadd5de5825513dff5b639bd6147f150e26
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-09-09 11:54:53 -0300

    Primer commit practica adonis

---
+++ my-app/start/routes.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+
+const Route = use('Route')
+
+Route.on('/').render('root')
+Route.on('/login').render('login')
+Route.get('/', () => 'Hello Adonis')
+
+
+Route.on('/failLogin').render('failLogin')
+Route.on('/signUp').render('signUp')
+Route.on('/logout').render('logout')
+Route.on('/productos').render('main')
+Route.on('/chat').render('about')
+
+  
+
+
+Route.on('*').render('error')
+
+
